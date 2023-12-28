5cbb376c00531fee57c3e3082503c46480e5e1a6
simplemind <karolispulokas@yahoo.com>
2023-12-28 20:28:54 +0000

    Update font families in main.css and defaults.js. Also moved Google Adsense and Analytics to separate includes with condition to work on process.env.NODE_ENV = 'production'

--- utilities/defaults.js
+++ utilities/defaults.js
@@ -1,13 +1,22 @@
 // Setting up default category, title and meta tags for pages
 // Not a complete list of tags - the ones used in homepage
+// const pageDefaultTags = {
+//   categoryTag: "gimtadienio", //For homepage
+//   pageTitle: "sveikinimai, gimtadienio sveikinimai, sveikinimai visoms progoms",
+//   pageHeadingH1: "Sveikinimai",
+//   pageSubheading:
+//     "Sveikinimai ir linkėjimai visoms progoms. Sveikinimų raktažodžiai: gimtadienio sveikinimai, sveikinimai gimtadienio proga, sveikinimai jubiliejaus proga, su gimtadieniu. Sveikinimai ir linkėjimai suskirstyti į įvairiausiais kategorijas. Iš viso daugiau kaip 2000 sveikinimų",
+//   metaDescription: "sveikinimai, sveikinimai ir linkėjimai visoms progoms - sveikinimai-linkejimai.com",
+//   metaKeywords: "sveikinimai, gimtadienio sveikinimai, sveikinimai gimtadienio proga, sveikinimai jubiliejaus proga, sveikinimai su gimtadieniu",
+// };
 const pageDefaultTags = {
-  categoryTag: "kalediniai", //For homepage
-  pageTitle: "sveikinimai, gimtadienio sveikinimai, sveikinimai visoms progoms",
-  pageHeadingH1: "Sveikinimai",
+  categoryTag: "naujametiniai", //For homepage
+  pageTitle: "naujametiniai sveikinimai, naujuju metu sveikinimai, naujuju metu<",
+  pageHeadingH1: "Naujametiniai sveikinimai",
   pageSubheading:
-    "Sveikinimai ir linkėjimai visoms progoms. Sveikinimų raktažodžiai: gimtadienio sveikinimai, sveikinimai gimtadienio proga, sveikinimai jubiliejaus proga, su gimtadieniu. Sveikinimai ir linkėjimai suskirstyti į įvairiausiais kategorijas. Iš viso daugiau kaip 2000 sveikinimų",
-  metaDescription: "sveikinimai, sveikinimai ir linkėjimai visoms progoms - sveikinimai-linkejimai.com",
-  metaKeywords: "sveikinimai, gimtadienio sveikinimai, sveikinimai gimtadienio proga, sveikinimai jubiliejaus proga, sveikinimai su gimtadieniu",
+    "Sveikinimai ir linkėjimai visoms progoms. Naujametiniai sveikinimai, Naujųjų metų sveikinimai, sveikinimai Naujųjų metų proga, Naujieji metai",
+  metaDescription: "naujametiniai sveikinimai, naujuju metu sveikinimai, sveikinimai naujuju metu proga, naujieji metai",
+  metaKeywords: "naujametiniai sveikinimai, naujuju metu sveikinimai, sveikinimai naujuju metu proga, naujieji metai",
 };
 
 module.exports = pageDefaultTags;
