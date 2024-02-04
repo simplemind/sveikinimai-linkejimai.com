bf6cdbcbdc3e58626be9c6aa8898e13c444a7e6a
simplemind <karolispulokas@yahoo.com>
2024-02-04 15:41:01 +0000

    Changed default homepage settings to romatiski ahead of Valentine's day

--- utilities/defaults.js
+++ utilities/defaults.js
@@ -1,23 +1,22 @@
 // Setting up default category, title and meta tags for pages
 // The ones used in homepage
-const pageDefaultTags = {
-  categoryTag: "gimtadienio", //For homepage
-  pageTitle: "sveikinimai, gimtadienio sveikinimai, sveikinimai visoms progoms",
-  pageHeadingH1: "Sveikinimai",
-  pageSubheading:
-    "Sveikinimai ir linkėjimai visoms progoms. Sveikinimų raktažodžiai: gimtadienio sveikinimai, sveikinimai gimtadienio proga, sveikinimai jubiliejaus proga, su gimtadieniu. Sveikinimai ir linkėjimai suskirstyti į įvairiausiais kategorijas. Iš viso daugiau kaip 2000 sveikinimų",
-  metaDescription: "sveikinimai, sveikinimai ir linkėjimai visoms progoms - sveikinimai-linkejimai.com",
-  metaKeywords: "sveikinimai, gimtadienio sveikinimai, sveikinimai gimtadienio proga, sveikinimai jubiliejaus proga, sveikinimai su gimtadieniu",
-};
-// Tags for an ocassion
 // const pageDefaultTags = {
-//   categoryTag: "naujametiniai", //For homepage
-//   pageTitle: "naujametiniai sveikinimai, naujuju metu sveikinimai, naujuju metu<",
-//   pageHeadingH1: "Naujametiniai sveikinimai",
+//   categoryTag: "gimtadienio", //For homepage
+//   pageTitle: "sveikinimai, gimtadienio sveikinimai, sveikinimai visoms progoms",
+//   pageHeadingH1: "Sveikinimai",
 //   pageSubheading:
-//     "Sveikinimai ir linkėjimai visoms progoms. Naujametiniai sveikinimai, Naujųjų metų sveikinimai, sveikinimai Naujųjų metų proga, Naujieji metai",
-//   metaDescription: "naujametiniai sveikinimai, naujuju metu sveikinimai, sveikinimai naujuju metu proga, naujieji metai",
-//   metaKeywords: "naujametiniai sveikinimai, naujuju metu sveikinimai, sveikinimai naujuju metu proga, naujieji metai",
+//     "Sveikinimai ir linkėjimai visoms progoms. Sveikinimų raktažodžiai: gimtadienio sveikinimai, sveikinimai gimtadienio proga, sveikinimai jubiliejaus proga, su gimtadieniu. Sveikinimai ir linkėjimai suskirstyti į įvairiausiais kategorijas. Iš viso daugiau kaip 2000 sveikinimų",
+//   metaDescription: "sveikinimai, sveikinimai ir linkėjimai visoms progoms - sveikinimai-linkejimai.com",
+//   metaKeywords: "sveikinimai, gimtadienio sveikinimai, sveikinimai gimtadienio proga, sveikinimai jubiliejaus proga, sveikinimai su gimtadieniu",
 // };
+// Tags for an ocassion
+const pageDefaultTags = {
+  categoryTag: "romantiski", //For homepage
+  pageTitle: "valentino sveikinimai, valentino dienos sveikinimai, valentino",
+  pageHeadingH1: "Romantiški sveikinimai",
+  pageSubheading: "Valentino sveikinimai, romantiški, valentino dienos sveikinimai, meilės palinkėjimai",
+  metaDescription: "valentino sveikinimai, romantiški, valentino dienos sveikinimai",
+  metaKeywords: "valentino sveikinimai, romantiški, valentino dienos sveikinimai, valentino dienos linkėjimai",
+};
 
 module.exports = pageDefaultTags;
