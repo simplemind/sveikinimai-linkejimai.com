388c427a9e3612b0c5cbbb52d373d12a9cf45927
simplemind <karolispulokas@yahoo.com>
2024-04-06 23:51:44 +0100

    Setting to default category from Velykos

--- utilities/defaults.js
+++ utilities/defaults.js
@@ -1,22 +1,22 @@
 // Setting up default category, title and meta tags for pages
 // The ones used in homepage
-// const pageDefaultTags = {
-//   categoryTag: "gimtadienio", //For homepage
-//   pageTitle: "sveikinimai, gimtadienio sveikinimai, sveikinimai visoms progoms",
-//   pageHeadingH1: "Sveikinimai",
-//   pageSubheading:
-//     "Sveikinimai ir linkėjimai visoms progoms. Sveikinimų raktažodžiai: gimtadienio sveikinimai, sveikinimai gimtadienio proga, sveikinimai jubiliejaus proga, su gimtadieniu. Sveikinimai ir linkėjimai suskirstyti į įvairiausiais kategorijas. Iš viso daugiau kaip 2000 sveikinimų",
-//   metaDescription: "sveikinimai, sveikinimai ir linkėjimai visoms progoms - sveikinimai-linkejimai.com",
-//   metaKeywords: "sveikinimai, gimtadienio sveikinimai, sveikinimai gimtadienio proga, sveikinimai jubiliejaus proga, sveikinimai su gimtadieniu",
-// };
-// Tags for an ocassion
 const pageDefaultTags = {
-  categoryTag: "velykiniai", //For homepage
-  pageTitle: "sveikinimai velykoms, velyku, velykiniai sveikinimai",
-  pageHeadingH1: "Sveikinimai Velykoms",
-  pageSubheading: "Sveikinimai Velykoms, Velykiniai sveikinimai, Velykų linkėjimai",
-  metaDescription: "sveikinimai velykoms, velykiniai sveikinimai, velyku",
-  metaKeywords: "sveikinimai velykoms, velyku, velykiniai sveikinimai, velykos, linkėjimai velykų proga, palinkėjimai velykom",
+  categoryTag: "gimtadienio", //For homepage
+  pageTitle: "sveikinimai, gimtadienio sveikinimai, sveikinimai visoms progoms",
+  pageHeadingH1: "Sveikinimai",
+  pageSubheading:
+    "Sveikinimai ir linkėjimai visoms progoms. Sveikinimų raktažodžiai: gimtadienio sveikinimai, sveikinimai gimtadienio proga, sveikinimai jubiliejaus proga, su gimtadieniu. Sveikinimai ir linkėjimai suskirstyti į įvairiausiais kategorijas. Iš viso daugiau kaip 2000 sveikinimų",
+  metaDescription: "sveikinimai, sveikinimai ir linkėjimai visoms progoms - sveikinimai-linkejimai.com",
+  metaKeywords: "sveikinimai, gimtadienio sveikinimai, sveikinimai gimtadienio proga, sveikinimai jubiliejaus proga, sveikinimai su gimtadieniu",
 };
+// Tags for an ocassion
+// const pageDefaultTags = {
+//   categoryTag: "velykiniai", //For homepage
+//   pageTitle: "sveikinimai velykoms, velyku, velykiniai sveikinimai",
+//   pageHeadingH1: "Sveikinimai Velykoms",
+//   pageSubheading: "Sveikinimai Velykoms, Velykiniai sveikinimai, Velykų linkėjimai",
+//   metaDescription: "sveikinimai velykoms, velykiniai sveikinimai, velyku",
+//   metaKeywords: "sveikinimai velykoms, velyku, velykiniai sveikinimai, velykos, linkėjimai velykų proga, palinkėjimai velykom",
+// };
 
 module.exports = pageDefaultTags;
