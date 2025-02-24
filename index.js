bbea805c8b60ea7cad9d4b17b41bdceeb9efe300
root <root@vps.server.local>
2025-02-24 00:34:29 +0000

    Updated CSP list of Urls for Helmet

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV!=="production") {
   // if we are in development require the dotenv package
   require("dotenv").config();
 }
-const express = require("express");
+const express=require("express");
 // Requiring NodeJs path module so I can set up an absolute path for views directory
-const path = require("path");
-const mongoose = require("mongoose"); //Requiring mongoose
+const path=require("path");
+const mongoose=require("mongoose"); //Requiring mongoose
 //Requiring the Category and Greeting schemas
 // Uppercased because it is a constructor function
-const Category = require("./models/categoriesModel");
-const Greeting = require("./models/greetingsModel");
+const Category=require("./models/categoriesModel");
+const Greeting=require("./models/greetingsModel");
 // DB_URL parameter from Production or local mongodb Url otherwise
-const dbUrl = "mongodb+srv://adminuser:Adminpass01@clustersveikinimai.uu5tqud.mongodb.net/dbSveikinimai";
+const dbUrl="mongodb+srv://adminuser:Adminpass01@clustersveikinimai.uu5tqud.mongodb.net/dbSveikinimai";
 // const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/dbSveikinimai";
 // const dbUrl = "mongodb+srv://adminuser:Adminpass01@clustersveikinimai.uu5tqud.mongodb.net/dbSveikinimai";
 // Importing an extended Error class for handling Express errors
-const ExpressError = require("./utilities/ExpressError");
-const ejsMate = require("ejs-mate"); //Importing ejs-mate npm package
+const ExpressError=require("./utilities/ExpressError");
+const ejsMate=require("ejs-mate"); //Importing ejs-mate npm package
 // For generating homepage greetings
-const pageDefaultTags = require("./utilities/defaults"); //Importing default values for the page
-const helmet = require("helmet"); //Importing helmet for managing security headers
+const pageDefaultTags=require("./utilities/defaults"); //Importing default values for the page
+const helmet=require("helmet"); //Importing helmet for managing security headers
 
 function handleError(error, res) {
   console.error(error);
@@ -30,9 +30,9 @@ function handleError(error, res) {
 // mongoose.connect(dbUrl).catch((error) => {
 //   handleError(error);
 // });
-const connectDb = async () => {
+const connectDb=async () => {
   try {
-    const conn = await mongoose.connect(dbUrl);
+    const conn=await mongoose.connect(dbUrl);
     console.log(`MongoDB connected: ${conn.connection.host}`);
     return conn;
   } catch (error) {
@@ -43,7 +43,7 @@ const connectDb = async () => {
 connectDb();
 
 // Getting an instance of Express
-const app = express();
+const app=express();
 
 // Joining root directory with the /views/ directory to form an absolute path
 // This tells Express where to find the views
@@ -62,25 +62,42 @@ app.engine("ejs", ejsMate);
 //  assigns pageTags and rootUrl without mutating the pageDefaultTags object
 app.use((req, res, next) => {
   // Constructing the rootUrl using req.get('host') and req.protocol
-  const rootUrl = new URL(`${req.protocol}://${req.get("host")}`);
+  const rootUrl=new URL(`${req.protocol}://${req.get("host")}`);
 
   // Adding rootUrl to res.locals to make it accessible in templates
-  res.locals.rootUrl = rootUrl;
+  res.locals.rootUrl=rootUrl;
 
   // Creating a copy of the default pageTags object
   // This ensures pageDefaultTags is not modified unlike cont pageTags = pageDefaultTags
-  const pageTags = Object.assign({}, pageDefaultTags);
+  const pageTags=Object.assign({}, pageDefaultTags);
   // Assigning pageTags to res.locals so it can be accessed in all views
-  res.locals.pageTags = pageTags;
+  res.locals.pageTags=pageTags;
 
   // Get environment from process.env.NODE_ENV
   // console.log(process.env.NODE_ENV);
-  res.locals.environment = process.env.NODE_ENV;
+  res.locals.environment=process.env.NODE_ENV;
   next();
 });
 
-// Calling middleware with Helmet
-const scriptSrcUrls = [
+// Setting CSP rules for Helmet
+const childSrcUrls=["*.googlesyndication.com", "*.adtrafficquality.google"];
+const connectSrcUrls=[
+  "https://ka-f.fontawesome.com",
+  "https://stats.g.doubleclick.net",
+  "https://www.google-analytics.com",
+  "https://fundingchoicesmessages.google.com",
+  "*.adtrafficquality.google",
+  "https://csi.gstatic.com"
+];
+const fontSrcUrls=["https://fonts.gstatic.com", "https://ka-f.fontawesome.com"];
+const imgSrcUrls=[
+  "https://stats.g.doubleclick.net",
+  "https://www.google.co.uk",
+  "https://www.google-analytics.com",
+  "*.adtrafficquality.google",
+  "https://csi.gstatic.com"
+];
+const scriptSrcUrls=[
   "https://kit.fontawesome.com/",
   "*.fontawesome.com",
   "*.google-analytics.com",
@@ -89,25 +106,23 @@ const scriptSrcUrls = [
   "*.google.com",
   "*.adtrafficquality.google",
 ];
-const styleSrcUrls = ["https://kit-free.fontawesome.com/", "https://fonts.googleapis.com/", "https://use.fontawesome.com/"];
-const connectSrcUrls = ["https://ka-f.fontawesome.com/"];
-const fontSrcUrls = ["https://fonts.gstatic.com/", "https://ka-f.fontawesome.com/"];
+const styleSrcUrls=["https://kit-free.fontawesome.com", "https://fonts.googleapis.com", "https://use.fontawesome.com"];
 
 //Calling middleware with helmet
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: [],
+      childSrc: ["blob:", ...childSrcUrls], // Allow/Block loading other web apps inside the site
       connectSrc: ["'self'", ...connectSrcUrls],
+      fontSrc: ["'self'", ...fontSrcUrls],
+      imgSrc: ["'self'", "blob:", "data:", ...imgSrcUrls],
+      mediaSrc: [],
+      objectSrc: [],
       scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls], // <script> elements & inline <script> blocks
       scriptSrcAttr: ["'unsafe-inline'", "'self'"], // For inline event handlers
       styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls], // CSS files
       workerSrc: ["'self'", "blob:"],
-      objectSrc: [],
-      imgSrc: ["'self'", "blob:", "data:"],
-      fontSrc: ["'self'", ...fontSrcUrls],
-      mediaSrc: [],
-      childSrc: ["blob:"], // Allow/Block loading other web apps inside the site
     },
   })
 );
@@ -116,28 +131,28 @@ app.use(
 // Fetching categories
 // The object contains categoryTag (used in Urls) , categoryName, orderNumber, pageTitle, pageHeadingH1, subheading
 async function getCategories() {
-  const categories = await Category.find({});
+  const categories=await Category.find({});
   // Sort the categories by orderNumber
   categories.sort((a, b) => {
-    return a.orderNumber - b.orderNumber;
+    return a.orderNumber-b.orderNumber;
   });
   return categories;
 }
 
 // A function that returns 10 greetings from the database
-async function getGreetings(categoryTag, page = 1, allFromFirst = false) {
-  const perPage = 10;
-  let skip = (page - 1) * perPage;
-  let limit = perPage;
+async function getGreetings(categoryTag, page=1, allFromFirst=false) {
+  const perPage=10;
+  let skip=(page-1)*perPage;
+  let limit=perPage;
 
   // If allFromFirst is true, fetch greetings from page 1 to the specified page
   if (allFromFirst) {
-    skip = 0;
-    limit = page * perPage;
+    skip=0;
+    limit=page*perPage;
   }
 
   // Fetches greetings from the database with a specified categoryTag
-  const arrayOfGreetingObjects = await Greeting.find({ categoryTags: { $in: [categoryTag] } })
+  const arrayOfGreetingObjects=await Greeting.find({ categoryTags: { $in: [categoryTag] } })
     .sort({ greetingOrderId: -1 })
     .skip(skip)
     .limit(limit);
@@ -148,28 +163,28 @@ async function getGreetings(categoryTag, page = 1, allFromFirst = false) {
 // A function that works out the number of pages for pagination
 // So we know when to stop pagination IntersectionObserver
 async function getNumberOfPages(categoryTag) {
-  const perPage = 10;
+  const perPage=10;
   // Fetches 10 greetings from the database with a specified categoryTag
-  const numberOfGreetings = await Greeting.find({ categoryTags: { $in: [categoryTag] } }).countDocuments();
-  const numberOfPages = Math.ceil(numberOfGreetings / perPage);
+  const numberOfGreetings=await Greeting.find({ categoryTags: { $in: [categoryTag] } }).countDocuments();
+  const numberOfPages=Math.ceil(numberOfGreetings/perPage);
   return numberOfPages;
 }
 
 // A function that updates the pageTags object with the values from the database
 async function updatePageTags(categoryTag, pageTags) {
-  const category = await Category.findOne({ categoryTag });
+  const category=await Category.findOne({ categoryTag });
 
   // create a copy of pageTags object to avoid mutation of pageTags object
-  const updatedPageTags = { ...pageTags };
+  const updatedPageTags={ ...pageTags };
 
-  updatedPageTags.categoryTag = category.categoryTag;
-  updatedPageTags.categoryName = category.categoryName;
-  updatedPageTags.pageTitle = category.pageTitle;
-  updatedPageTags.pageHeadingH1 = category.pageHeadingH1;
-  updatedPageTags.pageSubheading = category.pageSubheading;
-  updatedPageTags.metaDescription = category.metaDescription;
-  updatedPageTags.metaKeywords = category.metaKeywords;
-  updatedPageTags.onpageKeywords = category.onpageKeywords;
+  updatedPageTags.categoryTag=category.categoryTag;
+  updatedPageTags.categoryName=category.categoryName;
+  updatedPageTags.pageTitle=category.pageTitle;
+  updatedPageTags.pageHeadingH1=category.pageHeadingH1;
+  updatedPageTags.pageSubheading=category.pageSubheading;
+  updatedPageTags.metaDescription=category.metaDescription;
+  updatedPageTags.metaKeywords=category.metaKeywords;
+  updatedPageTags.onpageKeywords=category.onpageKeywords;
 
   // Return the modified copy of pageTags object
   return updatedPageTags;
@@ -181,16 +196,16 @@ async function updatePageTags(categoryTag, pageTags) {
 app.get("/", async (req, res) => {
   // Creating a copy of the default pageTags object
   // Destructuring from res.locals that was assigned by middleware
-  const { pageTags } = res.locals;
-  const currentCategory = pageTags.categoryTag;
+  const { pageTags }=res.locals;
+  const currentCategory=pageTags.categoryTag;
 
-  const page = req.query.puslapis || 1;
-  const categories = await getCategories();
-  const greetings = await getGreetings(pageTags.categoryTag, page, true);
-  const numberOfPages = await getNumberOfPages(pageTags.categoryTag);
+  const page=req.query.puslapis||1;
+  const categories=await getCategories();
+  const greetings=await getGreetings(pageTags.categoryTag, page, true);
+  const numberOfPages=await getNumberOfPages(pageTags.categoryTag);
 
   // Getting the root url for the page and environment from res.locals
-  const { rootUrl, environment } = res.locals;
+  const { rootUrl, environment }=res.locals;
 
   // Passing defaultCategory so it can be read by add-greetings.js
   res.render("home", { environment, categories, greetings, rootUrl, pageTags, currentCategory, numberOfPages });
@@ -199,17 +214,17 @@ app.get("/", async (req, res) => {
 // Page for selected category
 // page? means that page is optional
 app.get("/proga/:currentCategory", async (req, res) => {
-  const { currentCategory } = req.params;
+  const { currentCategory }=req.params;
   // Updating pageTags and preventing mutation of pageDefaultTags within the function
-  const pageTags = await updatePageTags(currentCategory, pageDefaultTags);
+  const pageTags=await updatePageTags(currentCategory, pageDefaultTags);
 
-  const page = req.query.puslapis || 1;
-  const categories = await getCategories();
-  const greetings = await getGreetings(pageTags.categoryTag, page, true);
-  const numberOfPages = await getNumberOfPages(pageTags.categoryTag);
+  const page=req.query.puslapis||1;
+  const categories=await getCategories();
+  const greetings=await getGreetings(pageTags.categoryTag, page, true);
+  const numberOfPages=await getNumberOfPages(pageTags.categoryTag);
 
   // Getting the root url for the page and environment from res.locals
-  const { rootUrl, environment } = res.locals;
+  const { rootUrl, environment }=res.locals;
 
   res.render("category", { environment, categories, greetings, rootUrl, pageTags, currentCategory, numberOfPages });
 });
@@ -218,10 +233,10 @@ app.get("/proga/:currentCategory", async (req, res) => {
 app.get("/privatumo-politika", async (req, res) => {
   // Creating a copy of the default pageTags object
   // Destructuring from res.locals that was assigned by middleware
-  const { pageTags } = res.locals;
+  const { pageTags }=res.locals;
 
   // Getting the root url for the page
-  const { rootUrl } = res.locals;
+  const { rootUrl }=res.locals;
 
   // Passing defaultCategory so it can be read by add-greetings.js
   res.render("privatumo-politika");
@@ -229,22 +244,22 @@ app.get("/privatumo-politika", async (req, res) => {
 
 // API route for fetching greetings
 app.get("/api/get-greetings/:category/:page?", async (req, res) => {
-  const { category, page } = req.params;
+  const { category, page }=req.params;
 
-  const greetings = await getGreetings(category, page);
+  const greetings=await getGreetings(category, page);
   res.json(greetings);
 });
 
 // sitemap.xml route
 app.get("/sitemap.xml", (req, res) => {
   // Getting the root url from res.locals
-  const { rootUrl } = res.locals;
+  const { rootUrl }=res.locals;
 
   // Start XML content
-  let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
-  xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+  let xml='<?xml version="1.0" encoding="UTF-8"?>\n';
+  xml+='<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
 
-  xml += `<url>
+  xml+=`<url>
           <loc>https://www.sveikinimai-linkejimai.com/</loc>
           <priority>1.0</priority>
         </url>
@@ -322,7 +337,7 @@ app.get("/sitemap.xml", (req, res) => {
         </url>`;
 
   // Close XML
-  xml += "</urlset>";
+  xml+="</urlset>";
 
   // Set content type to XML
   res.header("Content-Type", "application/xml");
@@ -340,14 +355,14 @@ app.all("*", (req, res, next) => {
 
 //Error handling middleware
 app.use(function (err, req, res, next) {
-  const { statusCode = 500 } = err; // Destructuring from the error with default values in case not passed by the error
-  if (!err.message) err.message = "SOMETHING WENT WRONG";
+  const { statusCode=500 }=err; // Destructuring from the error with default values in case not passed by the error
+  if (!err.message) err.message="SOMETHING WENT WRONG";
   // res.render("error"); //Renders error.ejs with err object passed through
   res.status(statusCode).render("error", { err }); //Renders error.ejs with err object passed through
 });
 
 // Listening on production for PORT. If not PORT then in development environment and use 3000
-const port = process.env.PORT || 3000;
+const port=process.env.PORT||3000;
 app.listen(3000, () => {
   console.log(`LISTENING ON PORT ${port}`);
 });
